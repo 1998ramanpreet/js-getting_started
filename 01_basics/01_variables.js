b73cbe6c62cdec20ae73f6c96095262eed39e138
Ramanpreet Kaur <1998ramanpreet@gmail.com>
2023-12-08 06:13:39 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail = "raman@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "rk@gmail.com"
+accountPassword="2122121221"
+accountCity = "Bengaluru"
+let accountState;
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// number =>2 to power 53
+// bigInt
+// string=> ""
+// boolean ==> true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
